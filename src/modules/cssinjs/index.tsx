fe468e5f824924463bb3f39eb2121bf23609c26e
LING_ZI_QING <onlylingstudio@gmail.com>
2023-04-19 09:15:22 +0800

    feat: 🎸 token/主题变更

--- src/modules/cssinjs/index.tsx
+++ src/modules/cssinjs/index.tsx
@@ -1,18 +1,47 @@
-import { memo } from 'react'
+import { memo, useState } from 'react'
 
 import CssInJsButton from './button'
+import type { DesignToken } from './theme'
 import { DesignTokenProvider, ThemeProvider } from './theme'
 
-const CssInJs = () => {
+const TOKEN_1: Partial<DesignToken> = {
+  primaryColor: '#f30',
+}
+
+const CssInJsChildren = () => {
   return (
-    <DesignTokenProvider>
-      <ThemeProvider>
-        <div>ddd</div>
-        <CssInJsButton>default</CssInJsButton>
-        <CssInJsButton type="dashed">dashed</CssInJsButton>
-        <CssInJsButton type="primary">primary</CssInJsButton>
-      </ThemeProvider>
-    </DesignTokenProvider>
+    <>
+      <div>ddd</div>
+      <CssInJsButton>default</CssInJsButton>
+      <CssInJsButton type="dashed">dashed</CssInJsButton>
+      <CssInJsButton type="primary">primary</CssInJsButton>
+    </>
+  )
+}
+
+const CssInJs = () => {
+  const [token, setToken] = useState<Partial<DesignToken> | undefined>()
+
+  return (
+    <>
+      <button
+        onClick={() => {
+          setToken(TOKEN_1)
+        }}>
+        token 1
+      </button>
+      <button
+        onClick={() => {
+          setToken(undefined)
+        }}>
+        token reset
+      </button>
+      <DesignTokenProvider hashed token={token}>
+        <ThemeProvider>
+          <CssInJsChildren />
+        </ThemeProvider>
+      </DesignTokenProvider>
+    </>
   )
 }
 
